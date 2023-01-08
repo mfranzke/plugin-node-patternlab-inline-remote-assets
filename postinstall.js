b174f769ce3023436a2f37429619267d63b06338
Maximilian Franzke <maximilian.franzke@deutschebahn.com>
2023-01-08 13:46:40 +0100

    feat: added postinstall script for running after the installation

---
+++ postinstall.js
@@ -0,0 +1,4 @@
+console.log(`Pattern Lab Node Plugin - "inline remote assets" installed.
+Use the CLI to install this: patternlab install --plugins plugin-node-patternlab-inline-remote-assets
+Configure or disable this plugin inside your patternlab-config.json file.
+`);
